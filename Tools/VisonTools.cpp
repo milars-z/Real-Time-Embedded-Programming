fd54abf4050762e8c67fc662696b0dfe25b11781
Ziyin Zeng <3113788Z@student.gla.ac.uk>
2026-02-12 15:15:19 +0000

    (MicrophoneTest)[ADD]:add function of getting device name

--- Tools/VisonTools.cpp
+++ Tools/VisonTools.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <string>
 
+#include <alsa/asoundlib.h>
+
 std::string extractText(const std::string& json) {
     size_t start = json.find("\"text\" : \"");
     if (start == std::string::npos) return "";
@@ -11,4 +13,25 @@ std::string extractText(const std::string& json) {
     size_t end = json.find("\"", start);
     if (end == std::string::npos) return "";
     return json.substr(start, end - start);
+}
+
+// is_capture: true for microphone, false for speaker
+std::string find_alsa_device(const std::string& keyword, bool is_capture) {
+    int card = -1;
+    char *name = nullptr;
+    
+    while (snd_card_next(&card) == 0 && card >= 0) {
+        // get device long name
+        if (snd_card_get_longname(card, &name) == 0) {
+            std::string longName(name);
+            free(name);
+            
+            // find keyword in long name
+            if (longName.find(keyword) != std::string::npos) {
+                
+                return (is_capture ? "plughw:" : "hw:") + std::to_string(card) + ",0";
+            }
+        }
+    }
+    return ""; 
 }
--- Tools/VisonTools.hpp
+++ Tools/VisonTools.hpp
@@ -3,5 +3,6 @@
 #include <string>
 
 std::string extractText(const std::string& json);
+std::string find_alsa_device(const std::string& keyword, bool is_capture);
 
 #endif
