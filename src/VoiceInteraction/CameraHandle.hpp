5e707aabe1f3d7e523643187e58c784545702f45
Ziyin Zeng <3113788Z@student.gla.ac.uk>
2026-03-16 17:18:03 +0000

    [MorMic] feat: add CameraHandle module edit after merging with CameraTest

---
+++ src/VoiceInteraction/CameraHandle.hpp
@@ -0,0 +1,12 @@
+// 该函数在VoiceInteraction中直接与Camera中的代码交流
+// 主要完成接受到text后控制图像识别相关的部分
+
+// 待CameraTest merge后调用
+
+
+#ifndef CAMERAHANDLE_HPP
+#define CAMERAHANDLE_HPP
+
+
+
+#endif // CAMERAHANDLE_HPP
