69a5e2f02e2c6d6b23ca59640c5b9da8bdbbc405
Ziyin Zeng <3113788Z@student.gla.ac.uk>
2026-04-09 19:01:06 +0100

    [fix] prevent multiple UI screens from being created

--- src/Screen/Screen_ui.cpp
+++ src/Screen/Screen_ui.cpp
@@ -1,20 +1,32 @@
 #include "Screen_ui.hpp"
 #include <iostream>
 
+// 结构体，装自己这个类的指针和btn的string，用来满足lvgl事件回调的要求
 struct DPadPayload {
-    ScreenUI* ui;          
-    std::string signalStr; 
+    ScreenUI* ui;  // 向函数内提供内部指针，发送信号用         
+    std::string signalStr;  // 信号字符串，由轮盘名称和btn上的方向组合成
 };
 
-ScreenUI::ScreenUI(ThreadSafeQueue<std::string>* outQueue) : signalQueue(outQueue) {
-    showHomeScreen(); // 启动时显示主页
+// 结构体，键盘发数据用
+struct ConfirmData {
+    ScreenUI* ui;  // 向函数内提供内部指针，发送信号用
+    std::string sig_type; // 信号类型，由调用键盘的地方决定
+    lv_obj_t* mask_to_del; // 遮罩对象，执行完确认后需要删除
+    lv_obj_t* ta; // 输入框
+};
+
+ScreenUI::ScreenUI(ScreenUI::UIEventCallback callback)
+    : onSignalEvent(callback) {
+    showHomeScreen();
 }
 
 void ScreenUI::sendSignal(std::string type, std::string data) {
-    std::string payload = type + ":" + data;
-    signalQueue->push(payload); 
-    printf("[ScreenUI] Sent signal: %s\n", payload.c_str());
-}
+        
+        if (onSignalEvent) {
+            onSignalEvent(type, data);
+        }
+        printf("[ScreenUI] Triggered callback: %s:%s\n", type.c_str(), data.c_str());
+    }
 
 // 初始界面 
 void ScreenUI::showHomeScreen() {
@@ -22,10 +34,10 @@ void ScreenUI::showHomeScreen() {
     // 关闭视觉界面状态
     is_in_vision_screen = false;
 
-    lv_obj_t* scr = lv_obj_create(NULL);
+    prepareMainScreen();
     
     // Motion 按钮
-    lv_obj_t* btn_m = lv_button_create(scr);
+    lv_obj_t* btn_m = lv_button_create(main_screen);
     lv_obj_set_size(btn_m, 200, 100);
     lv_obj_align(btn_m, LV_ALIGN_CENTER, -150, 0);
     lv_label_set_text(lv_label_create(btn_m), "Motion");
@@ -34,25 +46,26 @@ void ScreenUI::showHomeScreen() {
     }, LV_EVENT_CLICKED, this);
 
     // Vision 按钮
-    lv_obj_t* btn_v = lv_button_create(scr);
+    lv_obj_t* btn_v = lv_button_create(main_screen);
     lv_obj_set_size(btn_v, 200, 100);
     lv_obj_align(btn_v, LV_ALIGN_CENTER, 150, 0);
     lv_label_set_text(lv_label_create(btn_v), "Vision");
     lv_obj_add_event_cb(btn_v, [](lv_event_t* e){
         auto ui = (ScreenUI*)lv_event_get_user_data(e);
-        ui->sendSignal("VISION_STREAM_START");
+        // ui->sendSignal("VISION_STREAM_START");
         ui->showVisionScreen();
     }, LV_EVENT_CLICKED, this);
 
-    lv_screen_load(scr);
+    lv_screen_load(main_screen);
 }
 
 // Motion 界面 
 void ScreenUI::showMotionScreen() {
-    lv_obj_t* scr = lv_obj_create(NULL);
+
+    prepareMainScreen();
 
     // 返回键
-    lv_obj_t* btn_back = lv_button_create(scr);
+    lv_obj_t* btn_back = lv_button_create(main_screen);
     lv_obj_align(btn_back, LV_ALIGN_TOP_LEFT, 10, 10);
     lv_label_set_text(lv_label_create(btn_back), "Back");
     lv_obj_add_event_cb(btn_back, [](lv_event_t* e){
@@ -60,7 +73,7 @@ void ScreenUI::showMotionScreen() {
     }, LV_EVENT_CLICKED, this);
 
     // Learn 键 (右上)
-    lv_obj_t* btn_learn = lv_button_create(scr);
+    lv_obj_t* btn_learn = lv_button_create(main_screen);
     lv_obj_align(btn_learn, LV_ALIGN_TOP_RIGHT, -10, 10);
     lv_label_set_text(lv_label_create(btn_learn), "Learn");
     lv_obj_add_event_cb(btn_learn, [](lv_event_t* e){
@@ -68,11 +81,11 @@ void ScreenUI::showMotionScreen() {
     }, LV_EVENT_CLICKED, this);
 
     // 两个轮盘
-    createDPad(scr, -180, "left");
-    createDPad(scr, 180, "right");
+    createDPad(main_screen, -180, "left");
+    createDPad(main_screen, 180, "right");
 
     // OK 键 (中间)
-    lv_obj_t* btn_ok = lv_button_create(scr);
+    lv_obj_t* btn_ok = lv_button_create(main_screen);
     lv_obj_set_size(btn_ok, 80, 80);
     lv_obj_align(btn_ok, LV_ALIGN_CENTER, 0, 50);
     lv_label_set_text(lv_label_create(btn_ok), "OK");
@@ -80,21 +93,23 @@ void ScreenUI::showMotionScreen() {
         ((ScreenUI*)lv_event_get_user_data(e))->sendSignal("MOTION_CONFIRM");
     }, LV_EVENT_CLICKED, this);
 
-    lv_screen_load(scr);
+    lv_screen_load(main_screen);
 }
 
 // Vision 界面 
 void ScreenUI::showVisionScreen() {
-    is_in_vision_screen = true; 
-    lv_obj_t* scr = lv_obj_create(NULL);
-    lv_obj_set_style_bg_color(scr, lv_palette_main(LV_PALETTE_GREY), 0);
 
-    vision_img_obj = lv_image_create(scr);
+    is_in_vision_screen = true; 
+
+    prepareMainScreen();
+
+    vision_img_obj = lv_image_create(main_screen);
     lv_obj_set_size(vision_img_obj, 640, 480); // 假设视频大小
     lv_obj_align(vision_img_obj, LV_ALIGN_CENTER, -50, 0); 
 
 
     // 初始化描述符（假设 800 宽，根据 SDL 配置可能需要 RGB888）
+    memset(&vision_img_dsc, 0, sizeof(lv_image_dsc_t));
     vision_img_dsc.header.cf = LV_COLOR_FORMAT_RGB888;
     vision_img_dsc.header.w = 640;
     vision_img_dsc.header.h = 480;
@@ -105,18 +120,17 @@ void ScreenUI::showVisionScreen() {
     if(!canvas_buffer) canvas_buffer = (uint8_t*)malloc(640 * 480 * 3);
     vision_img_dsc.data = canvas_buffer;
 
-    lv_screen_load(scr);
-
+    lv_image_set_src(vision_img_obj, &vision_img_dsc);
 
     // Learn & Detect 
-    lv_obj_t* btn_l = lv_button_create(scr);
+    lv_obj_t* btn_l = lv_button_create(main_screen);
     lv_obj_align(btn_l, LV_ALIGN_TOP_RIGHT, -10, 10);
     lv_label_set_text(lv_label_create(btn_l), "Learn");
     lv_obj_add_event_cb(btn_l, [](lv_event_t* e){
         ((ScreenUI*)lv_event_get_user_data(e))->createKeyboard("VISION_LEARN");
     }, LV_EVENT_CLICKED, this);
 
-    lv_obj_t* btn_d = lv_button_create(scr);
+    lv_obj_t* btn_d = lv_button_create(main_screen);
     lv_obj_align(btn_d, LV_ALIGN_TOP_RIGHT, -10, 70);
     lv_label_set_text(lv_label_create(btn_d), "Detect");
     lv_obj_add_event_cb(btn_d, [](lv_event_t* e){
@@ -124,7 +138,7 @@ void ScreenUI::showVisionScreen() {
     }, LV_EVENT_CLICKED, this);
 
     // Update 
-    lv_obj_t* btn_u = lv_button_create(scr);
+    lv_obj_t* btn_u = lv_button_create(main_screen);
     lv_obj_align(btn_u, LV_ALIGN_TOP_RIGHT, -10, 130);
     lv_label_set_text(lv_label_create(btn_u), "Update");
     lv_obj_add_event_cb(btn_u, [](lv_event_t* e){
@@ -132,14 +146,14 @@ void ScreenUI::showVisionScreen() {
     }, LV_EVENT_CLICKED, this);
 
     // 返回
-    lv_obj_t* btn_back = lv_button_create(scr);
+    lv_obj_t* btn_back = lv_button_create(main_screen);
     lv_obj_align(btn_back, LV_ALIGN_TOP_LEFT, 10, 10);
     lv_label_set_text(lv_label_create(btn_back), "Back");
     lv_obj_add_event_cb(btn_back, [](lv_event_t* e){
         ((ScreenUI*)lv_event_get_user_data(e))->showHomeScreen();
     }, LV_EVENT_CLICKED, this);
 
-    lv_screen_load(scr);
+    lv_screen_load(main_screen);
 }
 
 void ScreenUI::createDPad(lv_obj_t* parent, int x_offset, std::string name) {
@@ -165,7 +179,7 @@ void ScreenUI::createDPad(lv_obj_t* parent, int x_offset, std::string name) {
         lv_obj_center(label);
 
         
-        // 配置信号格式
+        // 配置需要传入的结构体信息
         std::string full_sig = name + "_" + dirs[i];
         DPadPayload* payload = new DPadPayload{this, full_sig};
 
@@ -174,7 +188,7 @@ void ScreenUI::createDPad(lv_obj_t* parent, int x_offset, std::string name) {
             DPadPayload* p = (DPadPayload*)lv_event_get_user_data(e);
             
             // 信号发送
-            p->ui->sendSignal("Motion", p->signalStr);
+            p->ui->sendSignal("DO_MOTION", p->signalStr);
             
         }, LV_EVENT_CLICKED, payload);
 
@@ -220,28 +234,21 @@ void ScreenUI::createKeyboard(std::string signal_type) {
     lv_obj_set_size(kb_obj, 800, 240); // 占据下半屏
     lv_keyboard_set_textarea(kb_obj, ta_obj);
 
-    // 数据结构体
-    struct ConfirmData {
-        ScreenUI* ui;
-        std::string sig;
-        lv_obj_t* mask_to_del;
-        lv_obj_t* ta;
-    };
+
     ConfirmData* data = new ConfirmData{this, signal_type, mask, ta_obj};
 
-    // 信号传输回调
-    // 发送信号并销毁
+    // 确认键确认
     lv_obj_add_event_cb(btn_ok, [](lv_event_t* e) {
         ConfirmData* d = (ConfirmData*)lv_event_get_user_data(e);
         std::string content = lv_textarea_get_text(d->ta);
         
-        d->ui->sendSignal(d->sig, content);
+        d->ui->sendSignal(d->sig_type, content);
         
         lv_obj_delete(d->mask_to_del);
         delete d; 
     }, LV_EVENT_CLICKED, data);
 
-    // 键盘内的确定
+    // 键盘内的确定与取消事件
     lv_obj_add_event_cb(kb_obj, [](lv_event_t* e) {
         lv_event_code_t code = lv_event_get_code(e);
         if(code == LV_EVENT_READY) { 
@@ -249,7 +256,7 @@ void ScreenUI::createKeyboard(std::string signal_type) {
             lv_obj_t* ta = lv_keyboard_get_textarea(kb);
             ConfirmData* d = (ConfirmData*)lv_event_get_user_data(e);
             
-            d->ui->sendSignal(d->sig, lv_textarea_get_text(ta));
+            d->ui->sendSignal(d->sig_type, lv_textarea_get_text(ta));
             lv_obj_delete(d->mask_to_del);
             delete d;
         } else if(code == LV_EVENT_CANCEL) {
@@ -260,17 +267,25 @@ void ScreenUI::createKeyboard(std::string signal_type) {
     }, LV_EVENT_ALL, data);
 }
 
-void ScreenUI::updateVisionFrame(const cv::Mat& pre_processed_rgb) {
-    // 此时 pre_processed_rgb 已经是 640x480 的 RGB 格式
-    if (!is_in_vision_screen || pre_processed_rgb.empty() || !vision_img_obj) {
-        return;
-    }
+void ScreenUI::updateVisionFrame(const cv::Mat& frame) {
+    if (!is_in_vision_screen || frame.empty() || !vision_img_obj) return;
 
-    // 仅仅做一次内存拷贝，不再进行任何计算操作
-    // 如果内存对齐一致，memcpy 是最快的操作
-    memcpy(canvas_buffer, pre_processed_rgb.data, 640 * 480 * 3);
+    memcpy(canvas_buffer, frame.data, 640 * 480 * 3);
 
-    // 刷新显示
+    // 刷新逻辑
     lv_image_set_src(vision_img_obj, &vision_img_dsc);
     lv_obj_invalidate(vision_img_obj);
+}
+
+void ScreenUI::prepareMainScreen() {
+    if (!main_screen) {
+        main_screen = lv_obj_create(NULL);
+    } else {
+        lv_obj_clean(main_screen);
+    }
+
+    lv_obj_set_style_bg_color(main_screen, lv_color_white(), 0);
+    lv_obj_set_style_bg_opa(main_screen, LV_OPA_COVER, 0);
+    lv_obj_set_style_border_width(main_screen, 0, 0);
+    lv_obj_set_style_radius(main_screen, 0, 0);
 }
--- src/Screen/Screen_ui.hpp
+++ src/Screen/Screen_ui.hpp
@@ -1,17 +1,22 @@
 #ifndef SCREEN_UI_HPP
 #define SCREEN_UI_HPP
 
-#include "lvgl.h"
+#include "lvgl/lvgl.h"
 #include "ThreadSafeQueue.hpp"
 #include <cstdint> 
 #include <string>
 #include <opencv2/opencv.hpp>
+#include <atomic>
 
 
 class ScreenUI {
 public:
     
-    ScreenUI(ThreadSafeQueue<std::string>* outQueue);
+    // ScreenUI(ThreadSafeQueue<std::string>* outQueue);
+    
+    using UIEventCallback = std::function<void(std::string, std::string)>;
+
+    ScreenUI(UIEventCallback callback);
 
     // ScreenUI();
     
@@ -22,13 +27,16 @@ public:
     // 推流控制
     void updateVisionFrame(const cv::Mat& pre_processed_rgb);
 
-    bool is_in_vision_screen = false;   // 状态位
+    std::atomic<bool> is_in_vision_screen = false;   // 状态位
 
     ThreadSafeQueue<std::string>* signalQueue;
 
+    
+
 private:
     
-    
+    // 防止界面切换时内存泄露，创建一个屏幕指针，所有屏幕都用该指针
+    lv_obj_t* main_screen = nullptr;
 
     // LVGL 相关对象
     lv_obj_t* vision_img_obj = nullptr; // 图像显示组件
@@ -47,9 +55,14 @@ private:
     lv_obj_t* ta_obj = nullptr;
 
     // 图片显示相关
-    cv::Mat _ui_ready_frame;       // 预处理好的 RGB 图像
-    std::mutex _ui_frame_mtx;      // 保护预处理图像的锁
-    const cv::Size _ui_size = cv::Size(640, 480); // 目标 UI 尺寸
+    cv::Mat _ui_ready_frame;       
+    std::mutex _ui_frame_mtx;      
+    const cv::Size _ui_size = cv::Size(640, 480); 
+
+    UIEventCallback onSignalEvent; 
+
+    // 刷新界面时用
+    void prepareMainScreen();
 
 };
 
