595ac7b1fdfdb1328deaaea239f7ac8f3df2931d
Ziyin Zeng <3113788Z@student.gla.ac.uk>
2026-04-09 19:12:38 +0100

    [refactor] : update main for refactor - add exit_signal

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,40 +1,35 @@
-#include "voiceInteraction.hpp"
+// #include "voiceInteraction.hpp"
+#include "cogniArm.hpp"
 // #include "MotionManager.hpp"
 #include <iostream>
 #include <thread>
 #include <chrono>
 
-int main() {
-    RobotCore robot_interactive;
+// #include "Screen_ui.hpp"
+#include <unistd.h>
+#include <iostream>
 
-    if (!robot_interactive.init()) {
-        std::cerr << "Initialization failed." << std::endl;
+#include <csignal>
+#include <atomic>
+
+std::atomic<bool> _exit_signal = false;
+
+void handleSigint(int) {
+    _exit_signal = true;
+}
+
+
+int main() {
+    
+    // 退出信号处理
+    std::signal(SIGINT, handleSigint);
+    RobotSystem robot;
+
+    if (!robot.init()) {
         return -1;
     }
 
-    robot_interactive.start();
-
-    while (robot_interactive.running()) {
-
-        cv::Mat view = robot_interactive.getCamHandle()->getProcessedFrame();
-
-        if (!view.empty()) {
-            cv::imshow("Robot Camera Demo", view);
-        }
-
-        // Cam测试
-        int key = cv::waitKey(1); 
-        if (key == 'q') break;
-        
-        if (key == 'b') robot_interactive.getCamHandle()->Update_bg();
-        if (key == 's') robot_interactive.getCamHandle()->Learn_obj("obj_1");
-        if (key == 'm') robot_interactive.getCamHandle()->Find_obj("obj_1");
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-    }
-
-    robot_interactive.stop();
+    robot.start(); 
 
     return 0;
 }
